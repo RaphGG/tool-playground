ee5f2cab0ac2524ede7076189ae666563ce2e8f5
Raphael <raphg@knights.ucf.edu>
2020-04-15 21:42:37 -0400

    Switch bot changes

--- switch-bot/bot.py
+++ switch-bot/bot.py
@@ -5,97 +5,23 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect(("192.168.1.11", 6000))
 
 time.sleep(3)
-
+x = 0
 while True:
+  x+= 1
+  print(x)
+  s.sendall('click A\r\n'.encode())
+  time.sleep(0.2)
 
-  for y in range(12):
-    print(y)
-    for x in range(28):
-      print(x)
-      s.sendall('click A\r\n'.encode())
-      time.sleep(0.2)
-
-      s.sendall('click DLEFT\r\n'.encode())
-      time.sleep(0.2)
-
-      s.sendall('click DLEFT\r\n'.encode())
-      time.sleep(0.2)
-
-      s.sendall('click DLEFT\r\n'.encode())
-      time.sleep(0.2)
-
-      s.sendall('click DLEFT\r\n'.encode())
-      time.sleep(0.2)
-
-      s.sendall('click DLEFT\r\n'.encode())
-      time.sleep(0.2)
-
-
-      s.sendall('click DUP\r\n'.encode())
-      time.sleep(0.2)
-
-
-      s.sendall('click DRIGHT\r\n'.encode())
-      time.sleep(0.2)
-
-      s.sendall('click DRIGHT\r\n'.encode())
-      time.sleep(0.2)
-
-      s.sendall('click DRIGHT\r\n'.encode())
-      time.sleep(0.2)
-
-      s.sendall('click DRIGHT\r\n'.encode())
-      time.sleep(0.2)
-
-      s.sendall('click DRIGHT\r\n'.encode())
-      time.sleep(0.2)
-
-      s.sendall('click A\r\n'.encode())
-      time.sleep(0.2)
-    
-    s.sendall('click A\r\n'.encode())
-    time.sleep(0.2)
-
+  for y in range(5):
     s.sendall('click DLEFT\r\n'.encode())
     time.sleep(0.2)
 
-    s.sendall('click DLEFT\r\n'.encode())
-    time.sleep(0.2)
+  s.sendall('click DUP\r\n'.encode())
+  time.sleep(0.2)
 
-    s.sendall('click DLEFT\r\n'.encode())
-    time.sleep(0.2)
-
-    s.sendall('click DLEFT\r\n'.encode())
-    time.sleep(0.2)
-
-    s.sendall('click DLEFT\r\n'.encode())
-    time.sleep(0.2)
-
-    s.sendall('click DLEFT\r\n'.encode())
-    time.sleep(0.2)
-
-
-    s.sendall('click DUP\r\n'.encode())
-    time.sleep(0.2)
-
-
+  for y in range(5):
     s.sendall('click DRIGHT\r\n'.encode())
     time.sleep(0.2)
-
-    s.sendall('click DRIGHT\r\n'.encode())
-    time.sleep(0.2)
-
-    s.sendall('click DRIGHT\r\n'.encode())
-    time.sleep(0.2)
-
-    s.sendall('click DRIGHT\r\n'.encode())
-    time.sleep(0.2)
-
-    s.sendall('click DRIGHT\r\n'.encode())
-    time.sleep(0.2)
-
-    s.sendall('click DRIGHT\r\n'.encode())
-    time.sleep(0.2)
-
-    s.sendall('click A\r\n'.encode())
-    time.sleep(0.2)
+  
+  s.sendall('click A\r\n'.encode())
+  time.sleep(0.2)
